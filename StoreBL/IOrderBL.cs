ef1b414b905e23cbe301a19855e3e8fcc7d04178
Juniper Song <songminseonk@gmail.com>
2021-05-31 12:09:59 -0700

    Changed BL/DL logic to accept Guid instead of int for userId

--- StoreBL/IOrderBL.cs
+++ StoreBL/IOrderBL.cs
@@ -9,10 +9,10 @@ namespace StoreBL
 {
     public interface IOrderBL
     {
-        public Order GetOpenOrder(int customerId, int locationId);
+        public Order GetOpenOrder(Guid userId, int locationId);
         public Order CreateOrder(Order order);
-        public List<Order> GetOrdersByCustomerAndLocation(int customerId, int locationId);
-        public List<Order> GetOrdersByCustomerId(int customerId);
+        public List<Order> GetOrdersByCustomerAndLocation(Guid userId, int locationId);
+        public List<Order> GetOrdersByCustomerId(Guid userId);
         public List<LineItem> GetLineItemsByOrderId(int orderId);
         public LineItem CreateLineItem(LineItem item);
         public List<Order> GetOrdersByLocationId(int locationId);
