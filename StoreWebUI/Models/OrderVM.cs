bb6632912eb704b64185bf7297456a88575cadba
Juniper Song <songminseonk@gmail.com>
2021-05-31 15:20:08 -0700

    Order VM. May or maynot use

---
+++ StoreWebUI/Models/OrderVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using StoreModels;
+
+namespace StoreWebUI.Models
+{
+    public class OrderVM
+    {
+        public OrderVM() { }
+
+        public OrderVM(Order order)
+        {
+
+        }
+
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public Guid UserId { get; set; }
+        public int LocationId { get; set; }
+        public List<LineItem> LineItems { get; set; }
+    }
+}
