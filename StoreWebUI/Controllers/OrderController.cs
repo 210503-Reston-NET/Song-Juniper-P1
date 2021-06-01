ccda9740ad7612d3a3dba345863b016d1ef3451a
Juniper Song <songminseonk@gmail.com>
2021-05-31 17:22:16 -0700

    Updates the quantity of existing line item to the open order instead of keep creating duplicates

--- StoreWebUI/Controllers/OrderController.cs
+++ StoreWebUI/Controllers/OrderController.cs
@@ -24,6 +24,24 @@ namespace StoreWebUI.Controllers
             _locationBL = locationBL;
         }
 
+        public Order GetOpenOrder(int locationId, Guid userId)
+        {
+            //then, get the order with closed: false property associated to the current user's id and the location's id
+            Order openOrder = _orderBL.GetOpenOrder(userId, locationId);
+            //if there is no such order, create a new one and persist it to the db
+            if (openOrder is null)
+            {
+                Order order = new Order();
+                order.LocationId = locationId;
+                order.UserId = userId;
+                order.Closed = false;
+                openOrder = _orderBL.CreateOrder(order);
+            }
+            //Also get all lineItems associated to this order
+            openOrder.LineItems = _orderBL.GetLineItemsByOrderId(openOrder.Id) ?? new List<LineItem>();
+            return openOrder;
+        }
+
         // GET: CartController/5
         /// <summary>
         /// Shows the current "Open" order object associated to this location and the current customer
@@ -39,21 +57,9 @@ namespace StoreWebUI.Controllers
             Location currentLocation = _locationBL.FindLocationByID(id);
             //and store it in the viewbag
             ViewBag.CurrentLocation = currentLocation;
-            
-            //then, get the order with closed: false property associated to the current user's id and the location's id
-            Order openOrder = _orderBL.GetOpenOrder(new Guid(currentUserId), id);
-            //if there is no such order, create a new one and persist it to the db
-            if (openOrder is null)
-            {
-                Order order = new Order();
-                order.LocationId = id;
-                order.UserId = new Guid(currentUserId);
-                order.Closed = false;
-                openOrder = _orderBL.CreateOrder(order);
-            }
-            //Also get all lineItems associated to this order
-            openOrder.LineItems = _orderBL.GetLineItemsByOrderId(openOrder.Id) ?? new List<LineItem>();
 
+            //And get the open order for this store and the current user
+            Order openOrder = GetOpenOrder(id, new Guid(currentUserId));
             //finally, return the view with order.
             return View(openOrder);
         }
@@ -66,8 +72,27 @@ namespace StoreWebUI.Controllers
         /// <returns></returns>
         public ActionResult AddToCart(int id)
         {
-            InventoryVM item = new InventoryVM(_locationBL.GetInventoryById(id));
-            item.Quantity = 0;
+            //first of all, get the inventory item to add to the order
+            Inventory inventoryItem = _locationBL.GetInventoryById(id);
+            //And store the location data, so users can go back.
+            ViewBag.LocationId = inventoryItem.LocationId;
+
+            //and get the current user Id
+            string currentUserId = _userManager.GetUserId(User);
+            //And get the open order
+            Order openOrder = GetOpenOrder(inventoryItem.LocationId, new Guid(currentUserId));
+
+            //and see if the item we're trying to add is already in the "cart"
+            LineItem item = openOrder.LineItems.Find(item => item.Product.Id == inventoryItem.ProductId);
+            if(item is null)
+            {
+                //if we didn't find it, then initialize it from the inventory item.
+                item = new LineItem();
+                item.Product = inventoryItem.Product;
+                item.ProductId = inventoryItem.ProductId;
+                item.OrderId = openOrder.Id;
+                item.Quantity = 0;
+            }
             return View(item);
         }
 
@@ -75,30 +100,33 @@ namespace StoreWebUI.Controllers
         /// POST: LocationController/AddToCart/5
         /// persists the form data to db
         /// </summary>
-        /// <param name="id">inventory ID</param>
-        /// <param name="inventoryVM">form data</param>
+        /// <param name="inventoryId">inventory ID</param>
+        /// <param name="lineItem">form data</param>
         /// <returns></returns>
-        public ActionResult AddToCart(int id, InventoryVM inventoryVM)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddToCart(int inventoryId, LineItem lineItem)
         {
             try
             {
                 if (ModelState.IsValid)
                 {
-                    //Todo: Create order functionality first
-                    //and then get the "open" order and add to this.
-                    _orderBL.CreateLineItem(new LineItem
+                    if(lineItem.Id != 0)
                     {
-                        ProductId = inventoryVM.ProductId,
-                        //OrderId = order.Id,
-                        Quantity = inventoryVM.Quantity
-                    });
-                    return RedirectToAction(nameof(Inventory), "Location", new { id = inventoryVM.LocationId });
+                        //this item is already in the order, just update the quantity
+                        _orderBL.UpdateLineItem(lineItem);
+                    }
+                    else
+                    {
+                        _orderBL.CreateLineItem(lineItem);
+                    }
+                    return RedirectToAction(nameof(Inventory), "Location", new { id = _orderBL.GetOrderById(lineItem.OrderId).LocationId});
                 }
-                return AddToCart(id);
+                return AddToCart(inventoryId);
             }
             catch
             {
-                return AddToCart(id);
+                return AddToCart(inventoryId);
             }
         }
 
