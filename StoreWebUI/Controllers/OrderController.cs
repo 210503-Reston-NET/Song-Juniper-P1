7fdc63b57847af228d0c17d35990a73b7c770b48
Juniper Song <songminseonk@gmail.com>
2021-06-01 21:02:57 -0700

    You Shall Not Pass (..without authorization..)

--- StoreWebUI/Controllers/OrderController.cs
+++ StoreWebUI/Controllers/OrderController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -11,6 +12,7 @@ using StoreWebUI.Models;
 
 namespace StoreWebUI.Controllers
 {
+    [Authorize]
     public class OrderController : Controller
     {
         private readonly UserManager<User> _userManager;
--- StoreWebUI/Controllers/ProductController.cs
+++ StoreWebUI/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using StoreBL;
@@ -8,6 +9,7 @@ using StoreWebUI.Models;
 
 namespace StoreWebUI.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class ProductController : Controller
     {
         private IProductBL _productBL;
